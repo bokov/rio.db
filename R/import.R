f367340c050a7db6bebe750da56705707a0ebdd6
Alex Bokov <bokov@uthscsa.edu>
2019-10-07 16:41:54 -0500

    As per #5, renamed the 'table' argument for the export method to 'which', for consistency with core rio package

--- R/import.R
+++ R/import.R
@@ -1,12 +1,12 @@
-.import.rio_sqlite <- function(file, table, ...) {
+.import.rio_sqlite <- function(file, which, ...) {
     con <- DBI::dbConnect(RSQLite::SQLite(), file)
     on.exit(DBI::dbDisconnect(con))
     tabs <- DBI::dbListTables(con)
-    if (!missing(table)) {
-      if (is.numeric(table)) {
-          x <- DBI::dbReadTable(con, tabs[grep(table, tabs)[1]], ...)
+    if (!missing(which)) {
+      if (is.numeric(which)) {
+          x <- DBI::dbReadTable(con, tabs[grep(which, tabs)[1]], ...)
       } else {
-          x <- DBI::dbReadTable(con, table, ...)
+          x <- DBI::dbReadTable(con, which, ...)
       }
     } else {
         warning("Database contains multiple tables. Attempting first table.")
--- tests/testthat/test_sqlite.R
+++ tests/testthat/test_sqlite.R
@@ -1,11 +1,11 @@
 context("Export to SQLite")
 test_that("Export", {
-    expect_true(export(mtcars, "example.sqlite", table = "mtcars") %in% dir())
+    expect_true(export(mtcars, "example.sqlite", which = "mtcars") %in% dir())
 })
 
 context("Import from SQLite")
 test_that("Import", {
-    expect_true(is.data.frame(import("example.sqlite", table = "mtcars")))
+    expect_true(is.data.frame(import("example.sqlite", which = "mtcars")))
 })
 
 unlink("example.sqlite")
